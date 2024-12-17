6d1cf6769ab86a6980f1901396d1241a60e0a2ae
Jake <jacobj@spacelift.io>
2024-12-16 22:14:38 -0800

    added infracost api key context

---
+++ admin/contexts.tf
@@ -0,0 +1,13 @@
+resource "spacelift_context" "infracost" {
+  description = "config needed for infracost integration"
+  name        = "infraost-config"
+  labels      = ["autoattach:infracost"]
+  space_id    = root
+}
+
+resource "spacelift_environment_variable" "infracost_api_key" {
+  context_id = spacelift_context.ansible_context.id
+  name       = "INFRACOST_API_KEY"
+  value      = ""
+  write_only = true
+}

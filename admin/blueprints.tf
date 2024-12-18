2bbd68b1706851e578a94c69b521390e2ac7528e
apollorion <joey@apollorion.com>
2024-12-18 16:31:21 -0500

    feat: fix the blueprint

--- admin/blueprints.tf
+++ admin/blueprints.tf
@@ -2,6 +2,6 @@ resource "spacelift_blueprint" "pulumi_s3" {
   name        = "pulumi s3 for backend"
   description = "creates an s3 bucket for pulumi backend"
   space       = spacelift_space.pulumi.id
-  state       = "DRAFT"
+  state       = "PUBLISHED"
   template    = file("blueprints/pulumi-s3.yaml")
 }

be58cba12b860d68303abd30bdc4deddcff3d83b
Jake <jacobj@spacelift.io>
2025-01-09 11:56:11 -0800

    feat: add new ec2 instance

--- opentofu/ec2/main.tf
+++ opentofu/ec2/main.tf
@@ -9,3 +9,15 @@ resource "aws_instance" "spacelift_instance" {
     Name = "spacelift-node"
   }
 }
+
+resource "aws_instance" "spacelift_instance_two" {
+  ami                         = data.aws_ami.spacelift_server_ami.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnetId
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [var.aws_security_group_id]
+
+  tags = {
+    Name = "spacelift-node-two"
+  }
+}
